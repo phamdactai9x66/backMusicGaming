43c53a97c2e6306e5b144be3a483bed14c35885a
hoinc931 <c.hoivp55@gmail.com>
2021-08-29 00:57:16 +0700

    add topic

--- views/app/controllers/topicControllers.js
+++ views/app/controllers/topicControllers.js
@@ -1,4 +1,6 @@
+const formidable = require("formidable");
 const topic = require("../models/topic");
+let path = require('path');
 
 
 
@@ -9,6 +11,84 @@ class topics {
         console.log(getTopic)
         res.json({
             status: "this topic"
+        })
+    }
+
+    addNewTopic(req, res){
+        let form = new formidable.IncomingForm();
+        form.uploadDir = path.join(__dirname, "../../public/uploads");
+        form.keepExtensions = true;
+        form.maxFieldsSize = 1 * 1024 * 1024;
+        form.multiples = true;
+
+        form.parse(req, (err, fields, files) => {
+            if(err){
+                return res.status(400).json({
+                    message: "Error 400. Add New Topic"
+                })
+            }
+          
+            // check name topic = ''
+            if(fields.name === ''){
+                return res.json({
+                    message: "Name is required."
+                })
+            }
+
+            if(!files.image){
+                return res.json({
+                    message: "Image is required>"
+                })
+            }
+
+            let condition = {
+                // name: new RegExp(`${fields.name}`, 'i')
+                name: fields.name
+            }
+            
+            topic.findOne(condition).exec((err, topicExisted) => {
+                if(err){
+                    return res.json({
+                        message: "Error: " + err
+                    })
+                }
+                
+                if(topicExisted){
+                    return res.json({
+                        message: `this topic been exist in database`,
+                        result: topicExisted
+                    })
+                }
+
+                const uploadFile = files['image'];
+                
+                const indexOfPath = uploadFile.path.indexOf('upload')
+                
+                const cutPath = uploadFile.path.slice(indexOfPath);
+                
+                const checkImage = cutPath.split('.')[1];
+                if(checkImage === undefined){
+                    console.log('true')
+                    return res.json({
+                        message: "Image is required"
+                    })
+                }
+                
+                const data = {
+                    ...fields,
+                    image: `http://localhost:5000/${cutPath}`
+                }
+                let createTopic = new topic(data);
+                createTopic.save( (err, data) => {
+                    if(err){
+                        return res.json({
+                            message: err
+                        })
+                    }
+                    res.json({data})
+                })
+             
+            })
         })
     }
 }
--- views/routes/topic.js
+++ views/routes/topic.js
@@ -6,4 +6,6 @@ const topic_Controller = require("../app/controllers/topicControllers");
 
 
 router.get("/", topic_Controller.index);
+
+router.post('/', topic_Controller.addNewTopic)
 module.exports = router;
