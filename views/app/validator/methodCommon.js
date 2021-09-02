9a37af4fe5e20acac2fa6aefda7213dff1fde448
taipham231 <tai15122003311@gmail.com>
2021-09-02 17:27:21 +0700

    create route song get,put,delete,post song

--- views/app/validator/methodCommon.js
+++ views/app/validator/methodCommon.js
@@ -2,5 +2,7 @@
 module.exports = {
     statusS: "successfully",
     statusF: "failed",
-    localhost: "http://localhost:5000/"
+    localhost: "http://localhost:5000/",
+    extensionAudio: ["mp3", "wav", "flac", "aac"],
+    extensionImage: [".jpg", ".jpeg", ".bmp", ".gif", ".png"]
 }
--- views/routes/index.js
+++ views/routes/index.js
@@ -3,9 +3,10 @@ const side = require("./side");
 const topic = require("./topic");
 const songCate = require("./songCate");
 const playlist = require("./playlist");
+const song = require("./song");
 function routes(app) {
 
-
+      app.use("/song", song)
       app.use("/playlist", playlist)
       app.use("/songCate", songCate)
       app.use("/topic", topic)
