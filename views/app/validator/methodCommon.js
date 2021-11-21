45ab8cea7912c46651548a2a9bac2fbe568e128a
taipham231 <tai15122003311@gmail.com>
2021-11-21 23:48:45 +0700

    fix exception when auth

--- views/app/validator/methodCommon.js
+++ views/app/validator/methodCommon.js
@@ -203,8 +203,7 @@ const signFacebook = async (req, res, next) => {
 const checkLogin = async (req, res, next) => {
 
     try {
-        let get_token = req.headers.Authorization;
-
+        let get_token = req.headers.authorization;
         let cecode_token = decode_jwt(get_token, process.env.JWT_SECRET);
         const find_user = await modelUser.findOne({ _id: cecode_token.sub })
         if (find_user) {
